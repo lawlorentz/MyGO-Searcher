94e9b9853ce3e8fea7104f9bb95ef2fd6699324b
Carson <lawlorentz@gmail.com>
2024-11-16 13:42:28 -0500

    ok

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,11 +2,11 @@
     <div class="flex h-10 footer justify-center">
         <p class="text-sm">
             Built with <a :href="link" target="_blank" class="underline text-inherit">MyGo_Searcher</a> by Miyago9267, with modifications by
-            <a :href="link" target="_blank" class="underline text-inherit">Carson_L</a>
+            <a :href="link2" target="_blank" class="underline text-inherit">Carson_L</a>
         </p>
     </div>
     </template>
 <script setup lang="ts">
 const link = 'https://github.com/miyago9267/MyGo_Searcher'
-const link2 = 'https://github.com/lawlorentz'
+const link2 = 'https://github.com/lawlorentz/MyGO-Searcher'
 </script>
